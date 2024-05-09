f80bcd244eff062df6012eec80a9348dae2d2a84
暴风雨544 <1544007699@qq.com>
2024-05-09 16:11:18 +0800

    zero

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
 export default clerkMiddleware()
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
